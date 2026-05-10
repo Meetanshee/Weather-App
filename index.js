b0d58e38370f0f7a9878df933381a15df7c82942
Meetanshee Gupta <mitanshi.pari@gmail.com>
2026-05-11 00:23:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,39 +30,39 @@ if(weatherMain=="Clouds")
 {
     if(weatherIcon.includes("n"))
     {
-        image.src="cloudy-night-1.svg";
+        image.src="./cloudy-night-1.svg";
     }
     else
     {
-        image.src="cloudy-day-2.svg";
+        image.src="./cloudy-day-2.svg";
     }
 }
 else if(weatherMain=="Rain")
 {
-    image.src="rainy-1.svg";
+    image.src="./rainy-1.svg";
 }
 else if(weatherMain=="Clear")
 {
     if(weatherIcon.includes("n"))
     {
-        image.src="night.svg";
+        image.src="./night.svg";
     }
     else
     {
-        image.src="clear.svg";
+        image.src="./clear.svg";
     }
 }
 else if(weatherMain=="Snow")
 {
-    image.src="snowy-1.svg";
+    image.src="./snowy-1.svg";
 }
 else if(weatherMain=="Drizzle")
 {
-    image.src="rainy-2.svg";
+    image.src="./rainy-2.svg";
 }
 else if(weatherMain=="Thunderstorm")
 {
-    image.src="thunder.svg";
+    image.src="./thunder.svg";
 }
 else if(
     weatherMain=="Mist" ||
@@ -73,16 +73,16 @@ else if(
 {
     if(weatherIcon.includes("n"))
     {
-        image.src="cloudy-night-1.svg";
+        image.src="./cloudy-night-1.svg";
     }
     else
     {
-        image.src="cloudy.svg";
+        image.src="./cloudy.svg";
     }
 }
 else 
 {
-    image.src="clear.svg";
+    image.src="./clear.svg";
 }
 card.style="height:650px;transition:height 0.4s ease";
 weatherStatus.style="display:block";
